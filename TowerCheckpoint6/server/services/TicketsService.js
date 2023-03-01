7d63d505cf98bf1c3f74ebdb95fe3b03956a377b
Linda Taing <lindatdoan@me.com>
2023-03-01 13:44:41 -0700

    trying to getMy Tickets

--- TowerCheckpoint6/server/services/TicketsService.js
+++ TowerCheckpoint6/server/services/TicketsService.js
@@ -3,20 +3,21 @@ import { eventsService } from "../services/EventsService.js"
 import { Forbidden } from "../utils/Errors.js"
 
 class TicketsService {
+    async getMyTickets(accountId) {
+        const tickets = await dbContext.Tickets.find({ accountId })
+            .populate('profile')
+
+        return tickets
+    }
     async createTicket(ticketData, requestorId) {
         const event = await eventsService.getEventById(ticketData.eventId)
-
         if (event.isCanceled == true) {
             throw new Forbidden('Tickets cannot be bought for events that do not exist!')
         }
-
         const ticket = await dbContext.Tickets.create(ticketData)
-
         await ticket.populate('profile')
-
         const foundEvent = await eventsService.getEventById(ticketData.eventId)
         foundEvent.capacity--
-
 
         await foundEvent.save()
         return ticket
