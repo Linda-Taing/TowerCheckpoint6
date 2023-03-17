4071e7db4de041b57e7f95a142829bbe43ebf522
Linda Taing <lindatdoan@me.com>
2023-03-17 14:31:38 -0600

    updated for deployment

--- TowerCheckpoint6.client/vite.config.js
+++ TowerCheckpoint6.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'docs',
+    outDir: '../TowerCheckpoint6/client',
     sourcemap: false
   },
   server: {
